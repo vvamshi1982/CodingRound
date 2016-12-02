981ab5ef6f1ad86b8592e6fe2c54b3fcc3786da3
vvamshi1982 <vamshi.v.krishna@gmail.com>
2016-12-01 16:46:52 -0800

    Update SuperReducedString.cpp

--- SuperReducedString.cpp
+++ SuperReducedString.cpp
@@ -1,1 +1,57 @@
 
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	string InputStr,OutputStr;
+	stack<char> s;
+	int InputLen=0;
+		
+	cin >> InputStr;
+	InputLen = InputStr.size();
+    for (int i=0;i < InputLen;)
+    {
+       if (s.empty()) 
+           {
+  	   s.push(InputStr.at(i));
+           i++;
+       }
+  	 else if (InputStr.at(i) == s.top())
+  	 {
+  	    s.pop();
+  	    i++;
+  	 }
+  	 else
+  	  {
+  	   s.push(InputStr.at(i));
+  	   i++;
+  	   }
+       
+    }
+ 
+    if (s.empty())
+     cout << "Empty String";
+    else
+    { //Below can still be optimized
+     string reverse;
+	 while(!s.empty())
+	 {
+	 	reverse.push_back(s.top());
+	 	s.pop();
+	 }
+	 for (int i=reverse.size()-1 ;i >=0;i--)
+     {
+	    cout << reverse.at(i);
+	 }
+	 
+    }
+  
+    return 0;
+}
